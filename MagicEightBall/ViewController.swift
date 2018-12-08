a02af14bcd240c38c070399aea2b589e210bbac0
Lucas Dahl <LucasDahl425@yahoo.com>
2018-12-08 15:36:03 -0800

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Lucas Dahl on 12/8/18.
+//  Copyright © 2018 Lucas Dahl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var eightBall: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func selectBall() {
+        
+        // Get a radom number
+        let randomNumber = Int(arc4random_uniform(4) + 1)
+        
+        // Change teh image
+        eightBall.image = UIImage(named: "ball\(randomNumber)")
+        
+        
+    }
+
+    
+    //==================
+    // MARK: - Shake ball
+    //==================
+    
+    @IBAction func askTapped(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        
+        
+    }
+    
+}
+
