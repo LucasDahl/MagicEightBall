ae1b85c2a81f1a80c8bf2885f01ccd85e3d412d9
Lucas Dahl <LucasDahl425@yahoo.com>
2018-12-08 21:18:09 -0800

    Added an if statement that should make sure a new answer is given every time.

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     // Outlets
     @IBOutlet weak var eightBall: UIImageView!
     
+    // Properties
+    var oldRandomNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,11 +23,36 @@ class ViewController: UIViewController {
     
     func selectBall() {
         
-        // Get a radom number
-        let randomNumber = Int(arc4random_uniform(5) + 1)
+        // Method properties
+        var randomNumber = Int(arc4random_uniform(5) + 1)
         
         // Change the image
-        eightBall.image = UIImage(named: "ball\(randomNumber)")
+        if randomNumber != oldRandomNumber {
+            
+            // Pcik the new ball based on the random number
+            eightBall.image = UIImage(named: "ball\(randomNumber)")
+
+            // Set the new random number to the old random number
+            oldRandomNumber = randomNumber
+            
+        } else if oldRandomNumber == 1 && oldRandomNumber == randomNumber {
+            
+            // Add one to the random number so it is not out of range
+            randomNumber += 1
+
+            eightBall.image = UIImage(named: "ball\(randomNumber)")
+            
+        } else if oldRandomNumber == 5 && oldRandomNumber == randomNumber {
+            
+            // Subtract one to the random number so it is not out of range
+            randomNumber -= 1
+      
+            eightBall.image = UIImage(named: "ball\(randomNumber)")
+            
+        }
+        
+        
+        
         
     }
 
